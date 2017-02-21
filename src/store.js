4692a2e8781d116d4f1d29893ff60b0adafcd3e7
slindquist3 <slindquist3@gmail.com>
2017-02-21 14:49:15 -0500

    debugging

--- src/store.js
+++ src/store.js
@@ -3,7 +3,6 @@ import { composeWithDevTools } from 'redux-devtools-extension'
 import { createStore, applyMiddleware, compose } from 'redux'
 import rootReducer from './reducers/root_reducer'
 
-const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
-export default function configureStore(){
-  return createStore(rootReducer, composeEnhancers(applyMiddleware(ReduxPromise)))
-}
+export const store = createStore(rootReducer, composeWithDevTools(
+  applyMiddleware(ReduxPromise),
+))
