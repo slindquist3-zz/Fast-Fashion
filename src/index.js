3b087bfddf87fc581e2d229fedef378964bd892c
slindquist3 <slindquist3@gmail.com>
2017-02-21 14:13:53 -0500

    found more bugs in index.js

--- src/index.js
+++ src/index.js
@@ -8,10 +8,13 @@ import routes from './routes'
 
 import App from './containers/app';
 import rootReducer from './reducers/root_reducer';
+import configureStore from './store/configureStore';
+
+export const store = configureStore()
 
 
 
-const createStoreWithMiddleware = applyMiddleware(ReduxPromise)(createStore);
+// const createStoreWithMiddleware = applyMiddleware(ReduxPromise)(createStore);
 
 ReactDOM.render(
   <Provider store={createStoreWithMiddleware(rootReducer)}>
--- src/store.js
+++ src/store.js
@@ -1,9 +1,9 @@
-import thunk from 'redux-thunk'
+import ReduxPromise from 'redux-promise'
 import { composeWithDevTools } from 'redux-devtools-extension'
 import { createStore, applyMiddleware, compose } from 'redux'
 import rootReducer from './reducers/root_reducer'
 
 const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
 export default function configureStore(){
-  return createStore(rootReducer, composeEnhancers(applyMiddleware(thunk)))
+  return createStore(rootReducer, composeEnhancers(applyMiddleware(ReduxPromise)))
 }
