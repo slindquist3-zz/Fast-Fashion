6f8bc8445012e5dba779bef4508c8afc68d8db4f
slindquist3 <slindquist3@gmail.com>
2017-02-24 12:38:51 -0500

    added cors proxy to sessions actions

--- src/api/SessionApi.js
+++ src/api/SessionApi.js
@@ -3,14 +3,14 @@ import axios from 'axios'
 
 class SessionApi {
   static login(credentials) {
-      return axios.post("https://darling-api.herokuapp.com/login",
+      return axios.post("https://cors-anywhere.herokuapp.com/https://darling-api.herokuapp.com/login",
       JSON.stringify({user: credentials}),
       { headers: {'Content-Type': 'application/json'}
       })
     }
 
     static signUp(credentials) {
-      return axios.post("https://darling-api.herokuapp.com/users",
+      return axios.post("https://cors-anywhere.herokuapp.com/https://darling-api.herokuapp.com/users",
       JSON.stringify({user: credentials}),
       { headers: {'Content-Type': 'application/json'}
       })
