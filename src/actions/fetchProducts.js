962e9e1e822cd4f5669a32bbaa284179bc62f361
slindquist3 <slindquist3@gmail.com>
2017-02-23 21:13:42 -0500

    add s

--- src/actions/fetchProducts.js
+++ src/actions/fetchProducts.js
@@ -1,7 +1,7 @@
 import axios from 'axios'
 
 const API_KEY = "833625feb0218c764640681d15327431"
-const ROOT_URL = "http://api.prosperent.com/api/search?query="
+const ROOT_URL = "https://api.prosperent.com/api/search?query="
 
 export const FETCH_PRODUCTS = 'FETCH_PRODUCTS'
 
@@ -10,7 +10,7 @@ export function fetchProducts(search) {
     search = search.split(" ").join("+")
   }
 
-  const url = `https://cors-anywhere.herokuapp.com/http://api.prosperent.com/api/search?query=${search}&api_key=${API_KEY}`
+  const url = `https://cors-anywhere.herokuapp.com/https://api.prosperent.com/api/search?query=${search}&api_key=${API_KEY}`
   const request = axios.get(url)
 
   console.log('Request': request);
