ce2ab6bce371b920e0e761ec5b0d59c2512ea98f
slindquist3 <slindquist3@gmail.com>
2017-02-27 13:46:27 -0500

    added checkout to nav-bar

---
+++ src/components/checkout.js
@@ -0,0 +1,12 @@
+import React, {Component} from 'react';
+
+export default class Checkout extends Component {
+  render() {
+    return(
+      <div>
+        <h2>This isn't a checkout page, because this is just a portfolio piece!</h2>
+        <p>But... if you enjoyed your user experience, check out what the creator's up to over here</p>
+      </div>
+    )
+  }
+}
--- src/containers/nav_bar.js
+++ src/containers/nav_bar.js
@@ -13,7 +13,8 @@ export default class NavBar extends Component {
              <ul>
                <li><Link href="/" className="btn-floating black"><i className="material-icons">home</i></Link></li>
                <li><Link href="/login" className="btn-floating black"><i className="material-icons">vpn_key</i></Link></li>
-               <li><Link href="/search" className="btn-floating black"><i className="material-icons">shopping_cart</i></Link></li>
+               <li><Link href="/search" className="btn-floating black"><i className="material-icons">search</i></Link></li>
+               <li><Link href="/checkout" className="btn-floating black"><i className="material-icons">shopping_cart</i></Link></li>
              </ul>
            </div>
           </div>
