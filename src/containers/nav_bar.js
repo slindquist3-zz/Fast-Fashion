2be56cfdfadbec79cfb8aebfcfa81d6adc400ea2
slindquist3 <slindquist3@gmail.com>
2017-02-27 18:51:17 -0500

    changed color to teal

--- src/containers/nav_bar.js
+++ src/containers/nav_bar.js
@@ -7,13 +7,13 @@ export default class NavBar extends Component {
         return (
           <div className="float-button">
             <div className="float-button fixed-action-btn click-to-toggle">
-             <a className="float-button btn-floating btn-large red darken-4 waves-effect waves-light btn">
+             <a className="float-button btn-floating btn-large teal lighten-2 waves-effect waves-light btn">
                <i className="large material-icons">menu</i>
              </a>
              <ul>
-               <li><Link href="/" className="btn-floating red darken-3"><i className="material-icons">home</i></Link></li>
-               <li><Link href="/login" className="btn-floating red darken-2"><i className="material-icons">vpn_key</i></Link></li>
-               <li><Link href="/search" className="btn-floating red darken-1"><i className="material-icons">search</i></Link></li>
+               <li><Link href="/" className="btn-floating teal lighten-3"><i className="material-icons">home</i></Link></li>
+               <li><Link href="/login" className="btn-floating teal lighten-4"><i className="material-icons">vpn_key</i></Link></li>
+               <li><Link href="/search" className="btn-floating teal lighten-5"><i className="material-icons">search</i></Link></li>
              </ul>
            </div>
           </div>
