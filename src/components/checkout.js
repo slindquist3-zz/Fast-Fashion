107b8e09dc0af61a2eeab99d6e853c42f6080841
slindquist3 <slindquist3@gmail.com>
2017-02-27 14:08:49 -0500

    changed checkout

--- src/components/checkout.js
+++ src/components/checkout.js
@@ -7,10 +7,11 @@ export default class Checkout extends Component {
         <div className="col s6 offset-s2">
         <h2>This isn't a checkout page, because this is just a portfolio piece!</h2>
         <p>But... if you enjoyed your user experience,
-          check out what the creator's up to over <a href="https://scott-lindquist.com">here</a>
+          check out what the creator's up to over <a href="https://scott-lindquist.com">here!</a>
         </p>
         </div>
       </div>
     )
   }
 }
+//changed
