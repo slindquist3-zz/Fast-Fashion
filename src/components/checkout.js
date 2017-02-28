9a13195063fbac2066f2868e472711e9446e527a
slindquist3 <slindquist3@gmail.com>
2017-02-27 21:54:58 -0500

    editing shopping cart still

--- src/components/checkout.js
+++ src/components/checkout.js
@@ -6,12 +6,12 @@ export default class Checkout extends Component {
       <div className="row">
         <div className="col s12">
         <h3 className="center"><strong>Sorry! Checking out isn't available.</strong></h3>
-          <h5 className="center">[This is just a portfolio piece... I don't have the security to handle your real money.]</h5>
+          <h5 className="center">[ This is just a portfolio piece ]</h5>
           <div className="row">
-            <h5 className="center">See more of my work by clicking the button below.</h5>
+
 
             <div className="col s10 offset-s1">
-            <h5 className="center"> <a href="https://scott-lindquist.com" className="checkoutButton waves-effect waves-light btn">Click me to see more pretty things!</a>
+            <h5 className="center"> <a href="https://scott-lindquist.com" className="checkoutButton waves-effect waves-light btn">Click this button to see more of my work!</a>
             </h5>
           </div>
         </div>
