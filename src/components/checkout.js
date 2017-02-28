70ed918b7010a7287a934d63932c3f7569a66a88
slindquist3 <slindquist3@gmail.com>
2017-02-27 21:27:40 -0500

    fixed button

--- src/components/checkout.js
+++ src/components/checkout.js
@@ -8,8 +8,8 @@ export default class Checkout extends Component {
         <h3 className="center"><strong>Sorry! Checking out isn't available.</strong></h3>
           <h5 className="center">(This is just a portfolio piece.)</h5>
           <div className="row">
-            <div className="col s8">
-            <h5 className="center">But... if you enjoyed your user experience, check out what the creator's up to over     <a href="https://scott-lindquist.com" className="customButton waves-effect waves-light btn">here!</a>
+            <div className="col s10 offset-s1">
+            <h5 className="center">But... if you enjoyed your user experience, check out what the creator's up to over     <a href="https://scott-lindquist.com" className="checkoutButton waves-effect waves-light btn">here!</a>
             </h5>
           </div>
         </div>
