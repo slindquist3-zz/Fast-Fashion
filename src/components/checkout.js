c1fc79a4ad0e9cfc588b4843921c9de842d77cbf
slindquist3 <slindquist3@gmail.com>
2017-02-27 20:53:01 -0500

    messing with checkout page

--- src/components/checkout.js
+++ src/components/checkout.js
@@ -4,8 +4,8 @@ export default class Checkout extends Component {
   render() {
     return(
       <div className="row">
-        <div className="col s8 offset-s2">
-        <h3>This isn't a checkout page, because this is just a portfolio piece!</h3>
+        <div className="col s12">
+        <h4>This isn't a checkout page, because this is just a portfolio piece!</h4>
         <p>But... if you enjoyed your user experience,
           check out what the creator's up to over <a href="https://scott-lindquist.com">here!</a>
         </p>
