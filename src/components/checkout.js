d279fc18836e66daf85cc68bb1c08d72b6ef3cbc
slindquist3 <slindquist3@gmail.com>
2017-02-27 21:47:32 -0500

    more

--- src/components/checkout.js
+++ src/components/checkout.js
@@ -6,8 +6,10 @@ export default class Checkout extends Component {
       <div className="row">
         <div className="col s12">
         <h3 className="center"><strong>Sorry! Checking out isn't available.</strong></h3>
-          <h5 className="center">(This is just a portfolio piece.)</h5>
+          <h5 className="center">(This is just a portfolio piece, and I don't have the security to handle your real money.)</h5>
           <div className="row">
+            <h5 className="center">But if you like what you saw, you can see more by clicking the button below.</h5>
+
             <div className="col s10 offset-s1">
             <h5 className="center"> <a href="https://scott-lindquist.com" className="checkoutButton waves-effect waves-light btn">Check out my full website here!</a>
             </h5>
