ad818f2ea9bf174545821e1b2672a806db5b7697
slindquist3 <slindquist3@gmail.com>
2017-02-27 21:08:34 -0500

    added a button

--- src/components/checkout.js
+++ src/components/checkout.js
@@ -5,11 +5,12 @@ export default class Checkout extends Component {
     return(
       <div className="row">
         <div className="col s12">
-        <h4 className="center"><strong>Sorry! Checking out isn't available.</strong></h4>
-          <h5 className="center">(This is just a portfolio piece!)</h5>
-            <h6 className="center">But... if you enjoyed your user experience,
-          check out what the creator's up to over <a href="https://scott-lindquist.com">here!</a>
-        </h6>
+        <h2 className="center"><strong>Sorry! Checking out isn't available.</strong></h2>
+          <h4 className="center">(This is just a portfolio piece!)</h4>
+            <h5 className="center">But... if you enjoyed your user experience,
+              check out what the creator's up to over
+              <a href="https://scott-lindquist.com" className="customButton waves-effect waves-light btn">here!</a>
+            </h5>
       </div>
     </div>
     )
