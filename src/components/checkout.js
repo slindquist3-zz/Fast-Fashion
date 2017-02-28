eefee706a4a676e498f19e3f12a1f7d385199236
slindquist3 <slindquist3@gmail.com>
2017-02-27 21:01:58 -0500

    centering stuff

--- src/components/checkout.js
+++ src/components/checkout.js
@@ -5,8 +5,9 @@ export default class Checkout extends Component {
     return(
       <div className="row">
         <div className="col s12">
-        <h4><strong>This isn't a checkout page, because it's just a portfolio piece!</strong></h4>
-        <h6>But... if you enjoyed your user experience,
+        <h4 className="center"><strong>Sorry! Checking out isn't available.</strong></h4>
+          <h5 className="center">(This is just a portfolio piece!)<h5>
+            <h6 className="center">But... if you enjoyed your user experience,
           check out what the creator's up to over <a href="https://scott-lindquist.com">here!</a>
         </h6>
       </div>
