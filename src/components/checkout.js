a06728b44712c9a531c61a723457722fbcda235b
slindquist3 <slindquist3@gmail.com>
2017-02-27 21:03:21 -0500

    center

--- src/components/checkout.js
+++ src/components/checkout.js
@@ -6,7 +6,7 @@ export default class Checkout extends Component {
       <div className="row">
         <div className="col s12">
         <h4 className="center"><strong>Sorry! Checking out isn't available.</strong></h4>
-          <h5 className="center">(This is just a portfolio piece!)<h5>
+          <h5 className="center">(This is just a portfolio piece!)</h5>
             <h6 className="center">But... if you enjoyed your user experience,
           check out what the creator's up to over <a href="https://scott-lindquist.com">here!</a>
         </h6>
