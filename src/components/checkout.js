7c46fd67db9bb9e3b8d12c0b66a88a1ce1507a2e
slindquist3 <slindquist3@gmail.com>
2017-02-27 21:11:21 -0500

    changing formatting

--- src/components/checkout.js
+++ src/components/checkout.js
@@ -5,11 +5,10 @@ export default class Checkout extends Component {
     return(
       <div className="row">
         <div className="col s12">
-        <h2 className="center"><strong>Sorry! Checking out isn't available.</strong></h2>
-          <h4 className="center">(This is just a portfolio piece!)</h4>
+        <h3 className="center"><strong>Sorry! Checking out isn't available.</strong></h3>
+          <h5 className="center">(This is just a portfolio piece.)</h5>
             <h5 className="center">But... if you enjoyed your user experience,
-              check out what the creator's up to over
-              <a href="https://scott-lindquist.com" className="customButton waves-effect waves-light btn">here!</a>
+              check out what the creator's up to over     <a href="https://scott-lindquist.com" className="customButton waves-effect waves-light btn">here!</a>
             </h5>
       </div>
     </div>
