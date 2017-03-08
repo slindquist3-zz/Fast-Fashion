9027d5392cb9406f8d56bbc700ec4eeeff03d097
slindquist3 <slindquist3@gmail.com>
2017-03-08 10:34:02 -0500

    reverted

--- src/index.js
+++ src/index.js
@@ -3,10 +3,6 @@ import ReactDOM from 'react-dom';
 import { Router, browserHistory } from 'react-router'
 import routes from './routes'
 
-// import rootReducer from './reducers/root_reducer';
-// import configureStore from './store/configureStore';
-
-// export const store = configureStore()
 
 ReactDOM.render(
     <Router history={browserHistory} routes={routes} />
