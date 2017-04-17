2c271fc7298aae18e4b926d807bc69e602b1115b
Scott Lindquist <slindquist3@gmail.com>
2017-04-16 23:17:15 -0400

    Update index.js

--- src/index.js
+++ src/index.js
@@ -3,7 +3,7 @@ import ReactDOM from 'react-dom';
 import { Router, browserHistory } from 'react-router'
 import routes from './routes'
 
-
+//TEST
 ReactDOM.render(
     <Router history={browserHistory} routes={routes} />
   , document.querySelector('.container'));
